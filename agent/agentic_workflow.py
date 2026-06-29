00bc0d12084b132d84ad2018854de245260f9e28
Jaimin Trivedi <trivedij481@gmail.com>
2026-06-30 00:54:58 +0530

    updated code for weather tool implementation

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -1,26 +1,43 @@
+
 from utils.model_loader import ModelLoader
 from prompt_library.prompt import SYSTEM_PROMPT
-from langgraph.graph import StateGraph, START, END, MessagesState
+from langgraph.graph import StateGraph, MessagesState, END, START
 from langgraph.prebuilt import ToolNode, tools_condition
+from tools.weather_info_tool import WeatherInfoTool
+from tools.place_search_tool import PlaceSearchTool
+from tools.expense_calculator_tool import CalculatorTool
+from tools.currency_conversion_tool import CurrencyConverterTool
 
-# from tools.weather_info_tool import WeatherInfoTool
-# from tools.place_search_tool import PlaceSearchTool
-# from tools.expense_calculator_tool import ExpenseCalculatorTool
-# from tools.currency_conversion_tool import CurrencyConversionTool
-
-
-
-class Graph:
-    def __init__(self):
-        pass
+class GraphBuilder():
+    def __init__(self,model_provider: str = "groq"):
+        self.model_loader = ModelLoader(model_provider=model_provider)
+        self.llm = self.model_loader.load_llm()
+        
+        self.tools = []
+        
+        self.weather_tools = WeatherInfoTool()
+        self.place_search_tools = PlaceSearchTool()
+        self.calculator_tools = CalculatorTool()
+        self.currency_converter_tools = CurrencyConverterTool()
+        
+        self.tools.extend([* self.weather_tools.weather_tool_list, 
+                           * self.place_search_tools.place_search_tool_list,
+                           * self.calculator_tools.calculator_tool_list,
+                           * self.currency_converter_tools.currency_converter_tool_list])
+        
+        self.llm_with_tools = self.llm.bind_tools(tools=self.tools)
+        
+        self.graph = None
+        
+        self.system_prompt = SYSTEM_PROMPT
     
-     def agent_function(self,state: MessagesState):
+    
+    def agent_function(self,state: MessagesState):
         """Main agent function"""
         user_question = state["messages"]
         input_question = [self.system_prompt] + user_question
         response = self.llm_with_tools.invoke(input_question)
         return {"messages": [response]}
-    
     def build_graph(self):
         graph_builder=StateGraph(MessagesState)
         graph_builder.add_node("agent", self.agent_function)
@@ -31,8 +48,8 @@ class Graph:
         graph_builder.add_edge("agent",END)
         self.graph = graph_builder.compile()
         return self.graph
-    
-    def __call__(self): 
+        
+    def __call__(self):
         return self.build_graph()
 
     # __call__ method is used to make an instance of a class callable. It allows the instance to be called as if it were a function. When you call an instance of a class, Python will automatically invoke the __call__ method of that instance. In other words, it allows you to use the instance as a function, passing arguments to it and receiving a return value. This can be useful for creating objects that behave like functions or for implementing certain design patterns.
